fdaccf15b0186ab5496b28b975f6005ada80f52f
EvilGenius <evilgenius@211.ru>
2021-03-16 22:33:13 +0300

    Some projects created

---
+++ EvilGenius.Mvx.TabNavigation.Android/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace EvilGenius.Mvx.TabNavigation.Android
+{
+    public class Class1 : MvvmCross.Platforms.Android.Presenters.MvxAndroidViewPresenter
+    {
+        public Class1(IEnumerable<Assembly> androidViewAssemblies) : base(androidViewAssemblies)
+        {
+        }
+    }
+}
---
+++ EvilGenius.Mvx.TabNavigation.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EvilGenius.Mvx.TabNavigation.Core
+{
+    public class Class1
+    {
+    }
+}
